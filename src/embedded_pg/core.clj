0a1730737bc8340a0b0fd8bf396cbb561f221c0c
Andrew Lai <andrew.s.lai5@gmail.com>
2020-04-24 14:23:25 -0500

    Format nicely

--- src/embedded_pg/core.clj
+++ src/embedded_pg/core.clj
@@ -3,33 +3,50 @@
   (:import (io.zonky.test.db.postgres.embedded EmbeddedPostgres)))
 
 
-;; Run migrations if you have them
-#_(defn pg-db->migratus-config [db-spec]
-    {:migration-dirs "migrations"
-     :store :database
-     :db db-spec})
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start the actual POSTGRES instance, and retrieve info necessary to connect
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def test-pg (-> (EmbeddedPostgres/builder)
                  .start))
-
 (def ^:dynamic db-spec {:classname "org.postgresql.Driver"
                         :subprotocol "postgresql"
                         :subname (str "//localhost:" (.getPort test-pg) "/postgres")
                         :user "postgres"})
 
-;; Migratus again...
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Run migrations if you have them
+;;  this code is not included, you'll have to get migratus and
+;;  add to project.clj
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#_(defn pg-db->migratus-config [db-spec]
+    {:migration-dirs "migrations"
+     :store :database
+     :db db-spec})
 #_(m/migrate (pg-db->migratus-config db-spec))
 
-(jdbc/with-db-transaction [db db-spec]
-  (jdbc/query db-spec "SELECT * from fruit"))
-;;ERROR: relation "fruit" does not exist
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Playing with the DB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; What version of PG are we running? This can be modified using project.clj
 (jdbc/with-db-transaction [db db-spec]
   (jdbc/query db-spec "SELECT version()"))
 ;; => nil;;({:version PostgreSQL 10.6 on x86_64-apple-darwin, compiled by
 ;;i686-apple-darwin11-llvm-gcc-4.2 (GCC) 4.2.1 (Based on Apple Inc. build 5658)
 ;;(LLVM build 2336.11.00), 64-bit})
 
+
+;; Select from a non-existent table
+(jdbc/with-db-transaction [db db-spec]
+  (jdbc/query db-spec "SELECT * from fruit"))
+;;ERROR: relation "fruit" does not exist
+
+
+;; Add a new table inside a transaction that will rollback
+;; Prove that it actually creates and we can insert and select from it
 (jdbc/with-db-transaction [db db-spec]
   (jdbc/db-set-rollback-only! db)
   (binding [db-spec db]
@@ -47,6 +64,7 @@
 ;; => ({:name "apple", :appearance "Awesome", :cost 1, :grade 10.0})
 
 
+;; After the rollback, prove that the table doesn't exist anymore
 (jdbc/with-db-transaction [db db-spec]
   (jdbc/query db-spec "SELECT * from fruit"))
 ;; => ;;ERROR: relation "fruit" does not exist
